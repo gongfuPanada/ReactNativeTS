b61bd71a481316c1cb8506a51d42a1c4e1cb6542
Patricio López <lopezjuripatricio@gmail.com>
2015-11-18 14:29:18 -0300

    update location component

--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -5,6 +5,9 @@ const { StyleSheet, Text, View } = React;
 const Button = require("react-native-button");
 const Actions = require("react-native-router-flux").Actions;
 
+import {default as LocationStore, LocationState} from "../../stores/LocationStore";
+import LocationActions from "../../actions/LocationActions";
+
 
 const styles = StyleSheet.create({
     container: {
@@ -19,17 +22,23 @@ const styles = StyleSheet.create({
 });
 
 
-interface State {
-    locations: string[];
-}
-
-export class Locations extends React.Component<any, State> {
+export class Locations extends React.Component<any, LocationState> {
 
     constructor(props?: any) {
         super(props);
-        this.state = {
-            locations: ["Santiago, Chile"],
-        };
+        this.state = LocationStore.getState();
+    }
+
+    componentDidMount() {
+        LocationStore.listen(this.onChange);
+    }
+
+    componentWillUnmount() {
+        LocationStore.unlisten(this.onChange);
+    }
+
+    onChange(state: any) {
+        this.setState(state);
     }
 
     render() {
@@ -37,7 +46,7 @@ export class Locations extends React.Component<any, State> {
             <View style={styles.container}>
                 {this.state.locations.map(location => {
                     return (
-                        <Text>{location}</Text>
+                        <Text>{location.name}</Text>
                     );
                 })}
                 <Button style={styles.buttom} onPress={Actions.main}>
