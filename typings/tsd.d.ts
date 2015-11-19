300f508835c972f2c14e7d4be97cdda9e6209eb2
Patricio López <lopezjuripatricio@gmail.com>
2015-11-19 12:44:55 -0300

    remove es6-promise typings

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -2,4 +2,3 @@
 /// <reference path="react-native/react-native.d.ts" />
 /// <reference path="react/react.d.ts" />
 /// <reference path="alt/alt.d.ts" />
-/// <reference path="es6-promise/es6-promise.d.ts" />
